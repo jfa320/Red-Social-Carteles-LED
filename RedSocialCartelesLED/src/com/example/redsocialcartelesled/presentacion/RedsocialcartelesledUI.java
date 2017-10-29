b915230c0be03d94e33a925c7fb76903c51d9187
jfa320 <juarefa320@gmail.com>
2017-10-29 20:59:15 -0300

    Commit Original

---
+++ RedSocialCartelesLED/src/com/example/redsocialcartelesled/presentacion/RedsocialcartelesledUI.java
@@ -0,0 +1,29 @@
+package com.example.redsocialcartelesled.presentacion;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+@Theme("redsocialcartelesled")
+public class RedsocialcartelesledUI extends UI {
+
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = RedsocialcartelesledUI.class)
+	public static class Servlet extends VaadinServlet {
+	}
+
+	@Override
+	protected void init(VaadinRequest request) {
+		
+	}
+
+}
